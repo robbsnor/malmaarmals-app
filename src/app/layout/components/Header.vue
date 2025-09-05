a5f411895f32ae2afb0aa80666e9ef4aca1c0364
Robbin Schmidt <robbinschmidt@gmail.com>
2025-09-06 01:11:15 +0200

    -

--- src/app/layout/components/Header.vue
+++ src/app/layout/components/Header.vue
@@ -7,7 +7,7 @@ const appStore = useAppStore();
 <template>
     <div ref="header" class="top-0 right-0 left-0 z-100 bg-black/80 fixed backdrop-blur-md">
         <Container>
-            <div class="grid h-(--header-height) py-2" style="grid-template-columns: 1fr auto 1fr">
+            <div class="grid h-(--header-height) py-2 gap-4" style="grid-template-columns: 1fr auto 1fr">
                 <div class="flex items-center gap-8">
                     <RouterLink to="/" class="flex flex-col gap-[2px]">
                         <div class="text-2xl leading-[1] font-bold uppercase">malmaarmals</div>
--- src/app/videos/VideosPage.vue
+++ src/app/videos/VideosPage.vue
@@ -1,61 +1,16 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { supabase } from '../../supabase';
-import Filter from './components/Filter.vue';
-import type { Tables } from '../shared/types/database.types';
+import { storeToRefs } from 'pinia';
 import { useAppStore } from '../shared/stores/app.store';
-import { useDebounceFn } from '@vueuse/core';
+import Video from '../shared/components/Video.vue';
 
 const appStore = useAppStore();
-
-onMounted(async () => {
-    // videos.value = await fetchVideos();
-});
-
-const videos = ref<Tables<'videos'>[]>();
-
-const fetchVideos = async () => {
-    const { data, error } = await supabase
-        .from('videos')
-        .select('*')
-        .or(`title.ilike.%${appStore.searchQuery}%,description.ilike.%${appStore.searchQuery}%`);
-
-    if (error) {
-        console.error(error);
-        return;
-    }
-
-    videos.value = data;
-    console.log(data.length);
-};
-
-// const fetchVideos = async () => {
-//     const { data, error } = await supabase
-//         .from('videos')
-//         .select('*, categories: video_category_mapping(...categories(*))')
-//         .order('recorded_at', { ascending: false })
-//         .limit(1000);
-//     return { data, error };
-// };
-
-// const formattedDate = (date: string) => {
-//     return new Date(date).toLocaleDateString('en-GB', {
-//         day: 'numeric',
-//         month: 'long',
-//         year: 'numeric',
-//     });
-// };
+const { videos } = storeToRefs(appStore);
 </script>
 
 <template>
     <Container>
-        <code>
-            <pre>
-                {{ videos }}
-            </pre>
-        </code>
-        <!-- <div v-if="videos?.data" class="grid grid-cols-5 items-center gap-8 p-4 overflow-auto">
-            <Video v-for="video in videos.data" :key="video.video_id" :video="video" />
-        </div> -->
+        <div v-if="videos" class="grid grid-cols-5 items-center gap-8">
+            <Video v-for="video in videos" :key="video.video_id" :video="video" />
+        </div>
     </Container>
 </template>
