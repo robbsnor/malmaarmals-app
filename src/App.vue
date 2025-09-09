562d3a836996ddb4e1a8498a681292554849c112
Robbin Schmidt <robbinschmidt@gmail.com>
2025-09-09 06:32:33 +0200

    -

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,24 @@ import { useAppStore } from './app/shared/stores/app.store';
 </script>
 
 <template>
-    <Header />
+    <v-app>
+        <Header />
 
-    <div style="margin-top: var(--header-height)" class="min-h-available bg-pink-400f">
-        <RouterView />
-    </div>
+        <div style="margin-top: var(--header-height)" class="min-h-available bg-pink-400f">
+            <RouterView />
+        </div>
 
-    <!-- <Footer /> -->
+        <!-- <Footer /> -->
+        <Dev>
+            <div class="fixed bottom-8 right-8 z-50">
+                <v-speed-dial :open-on-hover="true" location="bottom center" transition="fade-transition">
+                    <template v-slot:activator="{ props: activatorProps }">
+                        <v-fab v-bind="activatorProps" icon="$vuetify"></v-fab>
+                    </template>
+
+                    <v-btn href="https://vuetifyjs.com/en/components/all/" target="_blank" icon="$vuetify"></v-btn>
+                </v-speed-dial>
+            </div>
+        </Dev>
+    </v-app>
 </template>
--- src/app/home/HomePage.vue
+++ src/app/home/HomePage.vue
@@ -4,59 +4,13 @@ import { computed, onMounted, ref } from 'vue';
 import Stats from '../../app/home/components/Stats.vue';
 import Video from '../app/../shared/components/Video.vue';
 import LatestVideo from './components/LatestVideo.vue';
+import Playlists from './components/Playlists.vue';
 import { useAppStore } from '../shared/stores/app.store';
 
 const appStore = useAppStore();
 const { videos } = storeToRefs(appStore);
 
 const number = ref(0);
-const playlists = ref([
-    {
-        title: 'Peter VS Timon',
-        episodes: 38,
-        thumbnails: [
-            'http://192.168.2.41:8000/thumbnails/2328589668',
-            'http://192.168.2.41:8000/thumbnails/2230408125',
-            'http://192.168.2.41:8000/thumbnails/2206408041',
-        ],
-    },
-    {
-        title: 'Straf Streams',
-        episodes: 24,
-        thumbnails: [
-            'http://192.168.2.41:8000/thumbnails/1901294175',
-            'http://192.168.2.41:8000/thumbnails/1888466252',
-            'http://192.168.2.41:8000/thumbnails/1681133900',
-        ],
-    },
-    {
-        title: 'Astro Bot',
-        episodes: 3,
-        thumbnails: [
-            'http://192.168.2.41:8000/thumbnails/2254460467',
-            'http://192.168.2.41:8000/thumbnails/2254460467',
-            'http://192.168.2.41:8000/thumbnails/2254460467',
-        ],
-    },
-    {
-        title: 'Mario Party',
-        episodes: 2,
-        thumbnails: [
-            'http://192.168.2.41:8000/thumbnails/2328589668',
-            'http://192.168.2.41:8000/thumbnails/2328589668',
-            'http://192.168.2.41:8000/thumbnails/2328589668',
-        ],
-    },
-    {
-        title: 'Teken streams',
-        episodes: 6,
-        thumbnails: [
-            'http://192.168.2.41:8000/thumbnails/2328589668',
-            'http://192.168.2.41:8000/thumbnails/2328589668',
-            'http://192.168.2.41:8000/thumbnails/2328589668',
-        ],
-    },
-]);
 
 const firstVideo = computed(() => videos?.value[number.value]);
 const previousVideos = computed(() => videos?.value.slice(1, 6));
@@ -89,44 +43,7 @@ onMounted(() => {
             </div>
         </Section>
 
-        <Section title="Playlists">
-            <div class="grid grid-cols-5 gap-8 pt-4">
-                <div
-                    v-for="playlist in playlists"
-                    :key="playlist.title"
-                    class="relative transition-all duration-200 rounded-md"
-                >
-                    <div class="relative transition-all duration-200 group">
-                        <img
-                            v-for="(thumbnail, i) in playlist.thumbnails"
-                            :key="i"
-                            class="absolute inset-0 rounded-md origin-top transition-all pointer-events-none"
-                            :src="thumbnail"
-                            aria-hidden="true"
-                            :class="[
-                                i === 2
-                                    ? '-top-2 scale-95 opacity-60 group-hover:-translate-y-1 group-hover:opacity-30'
-                                    : '',
-                                i === 1
-                                    ? '-top-4 scale-90 opacity-30 group-hover:-translate-y-2 group-hover:opacity-20'
-                                    : '',
-                            ]"
-                        />
-
-                        <VideoThumbnail
-                            :to="`/about`"
-                            :src="playlist.thumbnails[0]"
-                            icon="i-lucide-list-video"
-                            class="mb-2"
-                        >
-                        </VideoThumbnail>
-                    </div>
-
-                    <h2 class="font-bold text-md pt-2">{{ playlist.title }}</h2>
-                    <p class="text-text-muted text-sm font-medium">{{ playlist.episodes }} Episodes</p>
-                </div>
-            </div>
-        </Section>
+        <Playlists />
 
         <Stats />
 
