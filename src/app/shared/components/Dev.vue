48260bc5ea90a835ad43a8ae1502eb15d33f6159
Robbin Schmidt <robbinschmidt@gmail.com>
2025-09-09 10:42:21 +0200

    -

---
+++ src/app/shared/components/Dev.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const isDev = ref(process.env.NODE_ENV === 'development');
+</script>
+
+<template>
+    <template v-if="isDev">
+        <slot></slot>
+    </template>
+</template>
--- src/app/video/VideoPage.vue
+++ src/app/video/VideoPage.vue
@@ -47,8 +47,6 @@ onMounted(async () => {
     // await new Promise((resolve) => setTimeout(resolve, 1200));
     await getVideoInfo();
     loading.value = false;
-    await new Promise((resolve) => setTimeout(resolve, 8000));
-    chapters.value.pop();
 });
 
 const date = computed(() => {
@@ -85,16 +83,57 @@ const updateVideoTime = (e: any) => {
 </script>
 
 <template>
-    <div class="h-available md:flex gap-4 p-4">
-        <template v-if="videoInfo">
-            <div class="grow overflow-auto scrollbar-invisible">
-                <div class="aspect-video mx-auto" style="max-height: calc(100% - 0.4rem * 12)">
+    <div v-if="videoInfo" class="h-available flex flex-col">
+        <div>
+            <div class="bg-red-200">
+                <div class="aspect-video">
                     <Player :options="options" @timeupdate="updateVideoTime" ref="playerRef">
                         <source :src="`http://192.168.2.41:8000/videos/${videoInfo.video_id}`" type="video/mp4" />
                     </Player>
                 </div>
+            </div>
 
-                <div class="p-4 flex justify-between gap-4">
+            <div class="bg-green-200 rounded-md p-4 hidden">
+                <h2 class="text-2xl font-bold">{{ videoInfo.title }}</h2>
+                <h3 class="text-text-muted font-bold">
+                    {{ videoInfo.description }}
+                </h3>
+                <h3 class="text-text-muted">{{ date }}</h3>
+            </div>
+        </div>
+
+        <div>
+            <template>
+                <v-card>
+                    <v-tabs v-model="tab" bg-color="primary">
+                        <v-tab value="one">Item One</v-tab>
+                        <v-tab value="two">Item Two</v-tab>
+                        <v-tab value="three">Item Three</v-tab>
+                    </v-tabs>
+
+                    <v-card-text>
+                        <v-tabs-window v-model="tab">
+                            <v-tabs-window-item value="one"> One </v-tabs-window-item>
+
+                            <v-tabs-window-item value="two"> Two </v-tabs-window-item>
+
+                            <v-tabs-window-item value="three"> Three </v-tabs-window-item>
+                        </v-tabs-window>
+                    </v-card-text>
+                </v-card>
+            </template>
+        </div>
+        <Chat :videoId="Number(videoId)" :videoTime="videoTime" />
+
+        <!-- <div class="overflow-auto scrollbar-invisible rounded-md">
+            <div class="aspect-video mx-auto" style="max-height: calc(100% - 0.4rem * 12)">
+                <Player :options="options" @timeupdate="updateVideoTime" ref="playerRef">
+                    <source :src="`http://192.168.2.41:8000/videos/${videoInfo.video_id}`" type="video/mp4" />
+                </Player>
+            </div>
+
+            <div class="p-4">
+                <div class="flex justify-between gap-4">
                     <div>
                         <h2 class="text-2xl font-bold">{{ videoInfo.title }}</h2>
                         <h3 class="text-text-muted font-bold">
@@ -103,73 +142,20 @@ const updateVideoTime = (e: any) => {
                         <h3 class="text-text-muted">{{ date }}</h3>
                     </div>
 
-                    <div>
-                        <UPopover v-model:open="open" :content="{ avoidCollisions: true, side: 'top', align: 'end' }">
-                            <template #anchor>
-                                <v-btn label="Chapters" trailing-icon="i-lucide-chevron-down" @click="open = !open" />
-                            </template>
+                    <v-menu :location-strategy="{}">
+                        <template v-slot:activator="{ props }">
+                            <v-btn color="primary" v-bind="props"> Activator slot </v-btn>
+                        </template>
 
-                            <template #content>
-                                <div class="p-2 flex flex-col gap-1">
-                                    <button
-                                        @click="
-                                            seekToChapter(chapter.start_s);
-                                            open = false;
-                                        "
-                                        v-for="chapter in chapters"
-                                        class="flex gap-2 p-2 pr-2 items-center text-left cursor-pointer hover:bg-black-400 rounded-md"
-                                    >
-                                        <img :src="chapter.image_url" alt="" class="w-8 rounded-sm" />
-                                        <div>
-                                            <div>{{ chapter.title }}</div>
-                                            <div class="text-xs text-text-muted">
-                                                {{ formatSeconds(chapter.start_s) }}
-                                            </div>
-                                        </div>
-                                    </button>
-                                </div>
-                            </template>
-                        </UPopover>
-                    </div>
+                        <div class="p-4 bg-black-250">
+                            <div v-for="chapter in chapters" :key="chapter.start_s">
+                                <h4 class="text-lg font-bold">{{ chapter.title }}</h4>
+                                <img :src="chapter.image_url" alt="" class="w-50 rounded-md" />
+                            </div>
+                        </div>
+                    </v-menu>
                 </div>
             </div>
-
-            <div class="md:w-[300px] shrink-0">
-                <Chat :videoId="Number(videoId)" :videoTime="videoTime" />
-            </div>
-        </template>
-    </div>
-
-    <!-- <div
-            v-if="!loading"
-            class="4xl:grid-cols-[1fr_600px] grid grid-flow-col grid-cols-[1fr_300px] gap-4 overflow-hidden p-4"
-        >
-            <div class="scrollbar-invisible overflow-auto rounded-md bg-red-300f">
-                <template v-if="videoInfo">
-                    <div class="relative flex-1 rounded-md bg-blue-300">
-                        <Player :options="options" @timeupdate="updateVideoTime">
-                            <source :src="`http://192.168.2.41:8000/videos/${videoInfo.video_id}`" type="video/mp4" />
-                        </Player>
-                    </div>
-
-                    <div class="mt-4 rounded-md bg-pink-500f bg-red-200 p-4">
-                        <h2 class="text-2xl font-bold">{{ videoInfo.title }}</h2>
-                        <h3 class="text-text-muted font-bold">
-                            {{ videoInfo.description }}
-                        </h3>
-                        <h3 class="text-text-muted">{{ date }}</h3>
-                    </div>
-                </template>
-            </div>
-
-            <Chat :videoId="Number(videoId)" :videoTime="videoTime" />
         </div> -->
-
-    <!-- <div v-else class="h-available 4xl:grid-cols-[1fr_600px] grid grid-flow-col grid-cols-[1fr_300px] gap-4 p-4">
-        <div class="flex flex-col gap-4">
-            <USkeleton class="rounded-md flex-1" />
-            <USkeleton class="rounded-md h-20" />
-        </div>
-        <USkeleton class="rounded-md" />
-    </div> -->
+    </div>
 </template>
