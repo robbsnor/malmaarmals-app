395e4ea6374b301057ad0d481a5d325ef46f28b3
Robbin Schmidt <robbinschmidt@gmail.com>
2025-09-27 03:00:00 +0200

    -

--- src/app/video/components/InfoDesktop.vue
+++ src/app/video/components/InfoDesktop.vue
@@ -37,14 +37,41 @@ const prettyTime = (seconds: number) => {
 </script>
 
 <template>
-    <div class="hidden md:block overflow-hidden lg:mt-4 rounded-md">
-        <div class="pb-4 p-4 border-fb border-black-400">
-            <div class="font-bold text-lg">{{ videoInfo.title }}</div>
-            <div class="text-muted">{{ date }}</div>
+    <div class="hidden md:block overflow-hidden rounded-md">
+        <div class="flex justify-between items-center gap-4 pb-4 p-4">
+            <div class="">
+                <div class="font-bold text-lg">{{ videoInfo.title }}</div>
+                <div class="text-muted">{{ date }}</div>
+            </div>
+
+            <v-menu location="top right">
+                <template v-slot:activator="{ props }">
+                    <v-btn color="var(--color-black-500)" v-bind="props" prepend-icon="mdi-format-list-bulleted"
+                        >chapters</v-btn
+                    >
+                </template>
+
+                <div
+                    class="flex flex-col gap-2 bg-black-200 border border-black-400 h-100 rounded-md overflow-auto p-2"
+                >
+                    <button
+                        v-for="chapter in chapters"
+                        :key="chapter.start_s"
+                        @click="$emit('seekToChapter', chapter)"
+                        class="flex w-[300px] gap-2 p-3 bg-black-300 border border-black-500 rounded-md shrink-0 cursor-pointer text-left transition-all hover:bg-black-400"
+                    >
+                        <img :src="chapter.image_url" alt="chapter image" class="inline h-10 mr-2 rounded-md" />
+                        <div class="overflow-hidden">
+                            <div class="font-bold pr-2 truncate">{{ chapter.title }}</div>
+                            <div class="text-muted text-sm leading-3">{{ prettyTime(chapter.start_s) }}</div>
+                        </div>
+                    </button>
+                </div>
+            </v-menu>
         </div>
         <Stroke />
 
-        <div class="flex gap-4 overflow-auto p-4">
+        <!-- <div class="flex gap-4 overflow-auto p-4">
             <button
                 v-for="chapter in chapters"
                 :key="chapter.start_s"
@@ -57,6 +84,6 @@ const prettyTime = (seconds: number) => {
                     <div class="text-muted text-sm">{{ prettyTime(chapter.start_s) }}</div>
                 </div>
             </button>
-        </div>
+        </div> -->
     </div>
 </template>
