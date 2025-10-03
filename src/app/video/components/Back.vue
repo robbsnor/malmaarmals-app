f1242233ee9e6941590c77d675a42ef36e1a3911
Hopp <robbinschmidt@gmail.com>
2025-10-03 13:11:52 +0200

    -

---
+++ src/app/video/components/Back.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { useVideoStore } from '../stores/video.store';
+
+const router = useRouter();
+const videoStore = useVideoStore();
+</script>
+
+<template>
+    <button
+        @click="router.push('/videos')"
+        class="invisible -translate-y-2 transition-all opacity-0 absolute top-2 left-2 p-1 rounded-md bg-black/25 cursor-pointer hover:bg-black/70 lg:hidden"
+        :class="{ 'visible translate-y-0 opacity-100': videoStore.showInfo }"
+    >
+        <v-icon>mdi-chevron-left</v-icon>
+    </button>
+</template>
