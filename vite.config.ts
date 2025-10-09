8bc140cb091d6644a179f5f9f9adeea13c7161ec
robbsnor <robbinschmidt@gmail.com>
2025-10-10 00:39:46 +0200

    -

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,40 @@ export default defineConfig({
             devOptions: {
                 enabled: true,
             },
+
+            includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+            manifest: {
+                name: 'MallMaarMals',
+                short_name: 'MallMaarMals',
+                display: 'standalone',
+                theme_color: '#000000',
+                background_color: '#000000',
+                start_url: '/',
+                icons: [
+                    {
+                        src: 'pwa-64x64.png',
+                        sizes: '64x64',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any',
+                    },
+                    {
+                        src: 'maskable-icon-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'maskable',
+                    },
+                ],
+            },
         }),
         vuetify({
             styles: {
