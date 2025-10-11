8d92b25b5b442a6ca09f5abb846cd1982f310f3e
robbsnor <robbinschmidt@gmail.com>
2025-10-11 03:10:10 +0200

    -

--- src/app/video/components/MiniplayerControls.vue
+++ src/app/video/components/MiniplayerControls.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
-import Player from './components/Player.vue';
 import { useVideoStore } from '../stores/video.store';
 import { useAppStore } from '../../shared/stores/app.store';
 
