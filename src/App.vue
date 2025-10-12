eb14e297eba2301947264534d990909a6f764adc
robbsnor <robbinschmidt@gmail.com>
2025-10-12 10:10:38 +0200

    -

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,13 @@ const { top, right, bottom, left } = useScreenSafeArea();
 const appStore = useAppStore();
 
 const groups = [
-    {
-        items: [{ name: 'Prefferences', link: '/settings', icon: 'mdi-cog' }],
-    },
-
-    {
-        items: [
-            { name: 'Statistics', link: '/statistics', icon: 'mdi-chart-line' },
-            { name: 'About', link: '/about', icon: 'mdi-information' },
-            { name: 'Donate', link: '/donate', icon: 'mdi-heart' },
-        ],
-    },
-    {
-        items: [{ name: 'Sign out', link: '/sign-out', icon: 'mdi-logout' }],
-    },
+    [{ name: 'Prefferences', link: '/settings', icon: 'mdi-cog' }],
+    [
+        { name: 'Statistics', link: '/statistics', icon: 'mdi-chart-line' },
+        { name: 'About', link: '/about', icon: 'mdi-information' },
+        { name: 'Donate', link: '/donate', icon: 'mdi-heart' },
+    ],
+    [{ name: 'Sign out', link: '/sign-out', icon: 'mdi-logout' }],
 ];
 </script>
 
@@ -40,11 +33,11 @@ const groups = [
         <v-bottom-sheet v-model="appStore.bottomSheet" inset>
             <div class="bg-black-200 border border-black-400 p-4 rounded-t-md overflow-auto">
                 <div class="flex gap-4 flex-col">
-                    <div v-for="group in groups" :key="group.name">
+                    <div v-for="(group, index) in groups" :key="index">
                         <!-- <div class="text-gray-300 font-semibold mb-2">{{ group.name }}</div> -->
                         <div class="flex flex-col gap-0.5">
                             <button
-                                v-for="item in group.items"
+                                v-for="item in group"
                                 :key="item.name"
                                 @click="appStore.bottomSheet = false"
                                 class="w-full text-left px-4 py-3 bg-black-400 hover:bg-black-600 text-normal transition flex gap-3 cursor-pointer items-center first:rounded-t-md last:rounded-b-md"
