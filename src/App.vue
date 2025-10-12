1039f390cc7d0c09dc71154260393bc64ea4bb70
robbsnor <robbinschmidt@gmail.com>
2025-10-12 04:35:51 +0200

    -

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ const appStore = useAppStore();
 const groups = [
     {
         name: 'Main',
-        items: [{ name: 'Settings', link: '/settings', icon: 'mdi-cog' }],
+        items: [{ name: 'Prefferences', link: '/settings', icon: 'mdi-cog' }],
     },
     {
         name: 'User',
@@ -41,12 +41,12 @@ const groups = [
         <v-bottom-sheet v-model="appStore.bottomSheet" inset>
             <div class="bg-black-200 border border-black-400 p-4 rounded-t-md">
                 <div v-for="group in groups" :key="group.name" class="mb-4">
-                    <!-- <div class="text-gray-300 font-semibold mb-2">{{ group.name }}</div> -->
+                    <div class="text-gray-300 font-semibold mb-2">{{ group.name }}</div>
                     <div>
                         <button
                             v-for="item in group.items"
                             :key="item.name"
-                            @click="$router.push(item.link)"
+                            @click="appStore.bottomSheet = false"
                             class="w-full text-left px-3 py-2 mb-0.5 bg-black-400 hover:bg-black-600 text-normal transition flex gap-2 cursor-pointer items-center first:rounded-t-md last:rounded-b-md"
                             :class="item.name === 'Sign out' ? 'text-red-500' : ''"
                         >
