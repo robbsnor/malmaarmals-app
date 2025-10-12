7485f276a41bdc024588ca0fe49efec1f72bf474
robbsnor <robbinschmidt@gmail.com>
2025-10-12 04:35:51 +0200

    -

--- src/app/layout/components/MobileNavigation.vue
+++ src/app/layout/components/MobileNavigation.vue
@@ -2,14 +2,24 @@
 import { computed, ref } from 'vue';
 import { useAppStore } from '../../shared/stores/app.store';
 import { useScreenSafeArea } from '@vueuse/core';
+import { useRouter } from 'vue-router';
 
+const router = useRouter();
 const { top, right, bottom, left } = useScreenSafeArea();
 const appStore = useAppStore();
 const menuItems = ref([
     { title: 'home', icon: 'mdi-home', to: '/' },
     { title: 'streams', icon: 'mdi-play', to: '/videos' },
     { title: 'playlists', icon: 'mdi-playlist-play', to: '/about' },
+    { title: 'settings', icon: 'mdi-cog', to: '', isSettings: true },
 ]);
+
+const handleClick = (item: (typeof menuItems.value)[number]) => {
+    console.log(item);
+    if (item.isSettings) {
+        appStore.bottomSheet = true;
+    }
+};
 </script>
 
 <template>
@@ -22,13 +32,14 @@ const menuItems = ref([
         <div class="flex items-center justify-evenly h-[var(--height-mobile-navbar)]">
             <RouterLink
                 v-for="item in menuItems"
-                :to="item.to"
                 activeClass="text-white"
                 :key="item.title"
-                class="flex flex-col justify-center items-center text-muted transition-all gap-[2px] py-2 px-6 text-light"
+                :to="item.to"
+                @click="handleClick(item)"
+                class="cursor-pointer flex flex-col justify-center items-center text-muted transition-all gap-[2px] py-2 px-6 text-light"
             >
-                <v-icon :icon="item.icon" />
-                <div class="capitalize text-sm">{{ item.title }}</div>
+                <v-icon v-if="item.icon" :icon="item.icon" />
+                <div v-if="item.title" class="capitalize text-sm">{{ item.title }}</div>
             </RouterLink>
         </div>
     </div>
