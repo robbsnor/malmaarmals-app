a6fd7edb5786de18ca635eb4061a39c4ce83ae01
robbsnor <robbinschmidt@gmail.com>
2025-10-27 19:29:09 +0100

    -

--- src/app/video/components/MiniplayerControls.vue
+++ src/app/video/components/MiniplayerControls.vue
@@ -2,31 +2,36 @@
 import { useVideoStore } from '../stores/video.store';
 import { useAppStore } from '../../shared/stores/app.store';
 import { RouterLink, useRouter } from 'vue-router';
+import VideoButton from './VideoButton.vue';
 
 const videoStore = useVideoStore();
 const router = useRouter();
+
 function maximizePlayer() {
-    console.log('maximize');
     router.push({ name: 'video', params: { id: videoStore.videoId } });
     videoStore.player.isMini = false;
+}
+
+function togglePlay() {
+    videoStore.playing = !videoStore.playing;
 }
 </script>
 
 <template>
     <button
         v-if="videoStore.player.isMini"
-        class="absolute inset-0 flex justify-between p-2"
+        class="absolute inset-0 flex justify-between items-start p-2"
         @click.self="maximizePlayer"
     >
-        <v-btn icon @click="videoStore.playing = !videoStore.playing">
+        <VideoButton @click="togglePlay">
             <v-icon
-                size="32"
+                size="24"
                 :icon="videoStore.playing || (!videoStore.playing && videoStore.waiting) ? 'mdi-pause' : 'mdi-play'"
             />
-        </v-btn>
+        </VideoButton>
 
-        <v-btn icon @click="videoStore.reset">
-            <v-icon size="32" icon="mdi-close" />
-        </v-btn>
+        <VideoButton @click="videoStore.reset">
+            <v-icon size="24" icon="mdi-close" />
+        </VideoButton>
     </button>
 </template>
