2e5f151d3a1c976481296b8ee2600a72780ed6e5
robbsnor <robbinschmidt@gmail.com>
2025-10-27 19:29:09 +0100

    -

---
+++ src/app/video/components/VideoButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+</script>
+
+<template>
+    <div class="p-2 bg-black/50 rounded-full">
+        <slot></slot>
+    </div>
+</template>
