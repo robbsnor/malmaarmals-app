0b205b7051ed639830c39c1739e0a3bc6a13fdbf
robbsnor <robbinschmidt@gmail.com>
2025-10-28 19:00:23 +0100

    -

--- src/app/home/HomePage.vue
+++ src/app/home/HomePage.vue
@@ -5,12 +5,17 @@ import Playlists from './components/Playlists.vue';
 import { TitleHelper } from '../shared/helpers/title.helper';
 import PrevStreams from './components/PrevStreams.vue';
 import History from './components/History.vue';
+import { useAuthStore } from '../auth/stores/auth.store';
 
 TitleHelper.setTitle('home');
+const authStore = useAuthStore();
 </script>
 
 <template>
     <div>
+        <div>accessToken: {{ authStore.twitchAccessToken }}</div>
+        <div>refreshToken: {{ authStore.twitchRefreshToken }}</div>
+
         <Container>
             <div class="flex items-center justify-center py-12 flex-col gap-2 pb-6">
                 <div class="w-60">
