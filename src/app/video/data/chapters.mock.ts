cc98e7cec796a79bb65467d1d5f2d2c6e8db5dfc
robbsnor <robbinschmidt@gmail.com>
2025-11-02 18:59:57 +0100

    -

--- src/app/video/data/chapters.mock.ts
+++ src/app/video/data/chapters.mock.ts
@@ -1,37 +1,22 @@
 export const CHAPTERS_MOCK = [
     {
-        start_s: 1510,
-        title: 'Just Chatting',
+        start_s: 0,
+        title: 'Intro',
         image_url: 'https://static-cdn.jtvnw.net/ttv-boxart/509658-100x133.jpg',
     },
     {
-        start_s: 1800,
-        title: 'Mario Kart World',
+        start_s: 11 * 60 + 40,
+        title: 'Jacks Toys',
         image_url: 'https://static-cdn.jtvnw.net/ttv-boxart/1826300051_IGDB-100x133.jpg',
     },
     {
-        start_s: 2400,
-        title: 'Call of Duty: Black II',
-        image_url: 'https://static-cdn.jtvnw.net/ttv-boxart/489401_IGDB-100x133.jpg',
+        start_s: 47 * 60 + 42,
+        title: 'Greyhill Incident',
+        image_url: 'https://static-cdn.jtvnw.net/ttv-boxart/1826300051_IGDB-100x133.jpg',
     },
     {
-        start_s: 8720,
-        title: 'SilkSong',
-        image_url: 'https://static-cdn.jtvnw.net/ttv-boxart/511391_IGDB-100x133.jpg',
-    },
-    {
-        start_s: 12000,
-        title: 'Hollow Knight',
-        image_url: 'https://static-cdn.jtvnw.net/ttv-boxart/511391_IGDB-100x133.jpg',
-    },
-    {
-        start_s: 15000,
-        title: 'Among Us',
-        image_url: 'https://static-cdn.jtvnw.net/ttv-boxart/513510_IGDB-100x133.jpg',
-    },
-    {
-        start_s: 18000,
-        title: 'Just Chatting but a long name that is even longer',
-        image_url: 'https://static-cdn.jtvnw.net/ttv-boxart/509658-100x133.jpg',
+        start_s: 58 * 60 + 42,
+        title: 'Greyhill Incident (fout)',
+        image_url: 'https://static-cdn.jtvnw.net/ttv-boxart/1826300051_IGDB-100x133.jpg',
     },
 ];
