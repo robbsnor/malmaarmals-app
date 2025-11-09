507bf135b90cbd26efe7b5f30cc4e3495b0ff752
robbsnor <robbinschmidt@gmail.com>
2025-11-09 17:11:08 +0100

    -

--- src/app/video/components/Player.vue
+++ src/app/video/components/Player.vue
@@ -6,9 +6,11 @@ import { useAppStore } from '../../shared/stores/app.store';
 import MiniplayerControls from './MiniplayerControls.vue';
 import { BucketHelper } from '../../shared/helpers/bucket.helper';
 import PlayerControls from './PlayerControls.vue';
+import { usePreferenceStore } from '../../shared/stores/preference.store';
 
 const appStore = useAppStore();
 const videoStore = useVideoStore();
+const preferenceStore = usePreferenceStore();
 const videoRef = useTemplateRef<HTMLVideoElement>('videoRef');
 const { isFullscreen, enter, exit, toggle } = useFullscreen();
 
@@ -23,13 +25,29 @@ onMounted(async () => {
 
 <template>
     <div class="relative h-full w-full aspect-video flex flex-col items-center justify-center">
-        <video
-            preload="metadata"
-            class="aspect-video w-full"
-            ref="videoRef"
-            :src="videoStore.videoSrc"
-            :poster="BucketHelper.getThumbnailUrl(Number(videoStore.videoId))"
-        ></video>
+        <div class="relative aspect-video w-full">
+            <video
+                preload="metadata"
+                class="aspect-video w-full"
+                ref="videoRef"
+                :src="videoStore.videoSrc"
+                :poster="BucketHelper.getThumbnailUrl(Number(videoStore.videoId))"
+            />
+
+            <div
+                v-if="preferenceStore.preferences.showFacecam"
+                class="aspect-video w-[30%] absolute bottom-2 right-2 overflow-hidden rounded-md shadow-lg"
+            >
+                <img src="/images/facecam.jpg" class="object-fill w-full h-full" alt="Malse Facecam" />
+                <a
+                    class="absolute right-0 bottom-0 text-muted-more text-xs underline p-0.5"
+                    href="https://www.reddit.com/r/lekkerspelen/comments/1lhp8vc/peter_koopt_een_spijkerbroek/"
+                    target="_blank"
+                >
+                    @braxshinoa
+                </a>
+            </div>
+        </div>
 
         <div class="absolute inset-0" @click="videoStore.showControllsAndInfo = !videoStore.showControllsAndInfo"></div>
 
