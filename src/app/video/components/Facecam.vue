0516ca450286d96c29a282dfc0f051550f7a26ce
robbsnor <robbinschmidt@gmail.com>
2025-11-09 17:18:28 +0100

    -

---
+++ src/app/video/components/Facecam.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { usePreferenceStore } from '../../shared/stores/preference.store';
+
+const preferenceStore = usePreferenceStore();
+</script>
+
+<template>
+    <div
+        v-if="preferenceStore.preferences.showFacecam"
+        class="aspect-video w-[30%] absolute bottom-2 right-2 overflow-hidden rounded-md shadow-lg"
+    >
+        <img src="/images/facecam.jpg" class="object-fill w-full h-full" alt="Malse Facecam" />
+        <a
+            class="absolute right-1 bottom-0 text-muted text-xs p-0.5"
+            href="https://www.reddit.com/r/lekkerspelen/comments/1lhp8vc/peter_koopt_een_spijkerbroek/"
+            target="_blank"
+        >
+            @braxshinoa
+        </a>
+    </div>
+</template>
