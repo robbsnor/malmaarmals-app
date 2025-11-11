e24b094b2b8cba78e386696f22c4fea2561dad75
robbsnor <robbinschmidt@gmail.com>
2025-11-11 19:29:29 +0100

    -

--- src/app/video/components/VideoNotAllowed.vue
+++ src/app/video/components/VideoNotAllowed.vue
@@ -2,6 +2,7 @@
 import { useAuthStore } from '../../auth/stores/auth.store';
 import { useVideoStore } from '../stores/video.store';
 import SignInButton from '../../shared/components/SignInButton.vue';
+import { randomNumber } from '../../shared/helpers/randomNumber';
 
 const videoStore = useVideoStore();
 const authStore = useAuthStore();
@@ -10,10 +11,23 @@ const authStore = useAuthStore();
 <template>
     <div
         v-if="videoStore.player.isActive && !authStore.canWatch"
-        class="flex flex-col gap-4 p-4 md:flex-row bg-red-20f0 h-available"
+        class="flex flex-col gap-4 p-2 md:flex-row w-full h-available"
     >
-        <div class="bg-black-200 rounded-md w-full aspect-video md:aspect-auto"></div>
-        <div class="bg-black-200 rounded-md shrink-0 grow w-full md:w-[250px] md:grow-0"></div>
+        <SkeletonContainer class="flex flex-col gap-3 w-full md:flex-row">
+            <Skeleton :pulse="false" class="aspect-video w-full md:aspect-auto"></Skeleton>
+
+            <div class="flex flex-col gap-3 md:w-[350px]">
+                <div v-for="n in 40" :key="n">
+                    <Skeleton
+                        class="h-6"
+                        :pulse="false"
+                        :style="{
+                            width: randomNumber(40, 100) + '%',
+                        }"
+                    ></Skeleton>
+                </div>
+            </div>
+        </SkeletonContainer>
 
         <div class="absolute inset-0 p-8 flex items-center justify-center">
             <div class="bg-black-400 border border-black-500 rounded-md p-4">
