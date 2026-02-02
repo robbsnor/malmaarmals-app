e8c17b8c399d930945e487bda3d00ab3241ec3d2
robbsnor <robbinschmidt@gmail.com>
2026-02-02 07:02:06 +0100

    -

--- src/supabase.ts
+++ src/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './app/shared/models/database.types';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
