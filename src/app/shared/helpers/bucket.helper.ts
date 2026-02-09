48f683f7e3ea9b353ad63386dd0587843d6a99e5
robbsnor <robbinschmidt@gmail.com>
2026-02-09 01:19:10 +0100

    -

--- src/app/shared/helpers/bucket.helper.ts
+++ src/app/shared/helpers/bucket.helper.ts
@@ -1,3 +1,5 @@
+import { useAuthStore } from '../../auth/stores/auth.store';
+
 export class BucketHelper {
     static BUCKET_URL = import.meta.env.VITE_BUCKET_URL;
 
@@ -5,7 +7,15 @@ export class BucketHelper {
         return `${BucketHelper.BUCKET_URL}/thumbnails/${id}`;
     }
 
-    static getVideoUrl(rotatingId: string) {
-        return `${BucketHelper.BUCKET_URL}/videos/${rotatingId}`;
+    static async getVideoUrl(videoId: number): Promise<{ signedUrl: string }> {
+        const authStore = useAuthStore();
+        const twitchUserId = authStore.session?.user?.user_metadata?.provider_id;
+        if (!twitchUserId) throw new Error('User not authenticated');
+
+        const data = await fetch(`${BucketHelper.BUCKET_URL}/generate-video-url`, {
+            method: 'POST',
+            body: JSON.stringify({ videoId: videoId, twitchUserId }),
+        });
+        return await data.json();
     }
 }
