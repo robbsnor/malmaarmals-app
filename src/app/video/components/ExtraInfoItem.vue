2be0d59e7f454128370946915be8e350cd10bf15
robbsnor <robbinschmidt@gmail.com>
2026-02-13 06:33:53 +0100

    -

--- src/app/video/components/ExtraInfoItem.vue
+++ src/app/video/components/ExtraInfoItem.vue
@@ -8,7 +8,7 @@ const props = withDefaults(
 </script>
 
 <template>
-    <div class="bg-black-200 rounded-lg overflow-hidden max-h-[80vh]">
+    <div class="bg-black-200 rounded-lg overflow-hidden">
         <div class="flex justify-between border-b border-black-400 px-4 py-3 items-end">
             <div class="font-bold text-2xl">
                 {{ title }}
