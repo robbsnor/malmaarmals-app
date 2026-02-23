942bd2221d7edc59dc1acc594f612d80edb6d95c
robbsnor <robbinschmidt@gmail.com>
2026-02-24 00:21:48 +0100

    -

--- src/app/video/components/ExtraInfoChapters.vue
+++ src/app/video/components/ExtraInfoChapters.vue
@@ -4,6 +4,7 @@ import { useVideoStore } from '../stores/video.store';
 import ExtraInfoItem from './ExtraInfoItem.vue';
 import { useAuthStore } from '../../auth/stores/auth.store';
 import CategoryThumbnail from './CategoryThumbnail.vue';
+import Chapters from './Chapters.vue';
 
 const videoStore = useVideoStore();
 const authStore = useAuthStore();
@@ -12,19 +13,7 @@ const authStore = useAuthStore();
 <template>
     <ExtraInfoItem v-if="videoStore.chapters.length" title="Chapters">
         <div v-if="videoStore.chapters?.length" class="flex flex-col gap-2 p-4">
-            <button
-                v-for="chapter in videoStore.chapters"
-                :key="chapter.start_s"
-                class="flex items-center gap-4 p-3 bg-black-300 border border-black-500 rounded-md shrink-0 text-left transition-all hover:bg-black-400"
-            >
-                <CategoryThumbnail :category="chapter.category" />
-
-                <div class="overflow-hidden">
-                    <div class="font-bold pr-2 truncate">{{ chapter.category.title }}</div>
-                    <div class="text-muted text-sm"></div>
-                </div>
-                <v-icon class="ml-auto" color="var(--color-text-muted)">mdi-chevron-right</v-icon>
-            </button>
+            <Chapters />
         </div>
 
         <Empty v-else title="No chapters yet..." icon="mdi-format-list-bulleted"> </Empty>
