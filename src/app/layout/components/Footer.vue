11a9cf3209bc8084a1ddd5002dac6cdc0ebb4ef0
robbsnor <robbinschmidt@gmail.com>
2026-03-03 00:25:07 +0100

    -

--- src/app/layout/components/Footer.vue
+++ src/app/layout/components/Footer.vue
@@ -3,10 +3,15 @@
 <template>
     <div class="lg:bg-black border-t border-black-400">
         <Container>
-            <div class="flex flex-col items-center text-muted-more py-10">
-                <div>
-                    Made with <v-icon icon="mdi-heart-outline" class="text-primary animate-pulse -mt-1 mx-1 mr-2" />
-                    <a href="https://www.twitch.tv/robbsnor" target="_blank" class="hover:underline">Robbsnor</a>
+            <div class="flex justify-center items-center">
+                <div class="inline-grid grid-cols-[1fr_auto_1fr] gap-1 items-center text-muted-more py-10">
+                    <div class="place-self-end">Made with</div>
+
+                    <v-icon icon="mdi-heart-outline" class="text-primary animate-pulse -mt-1 mx-1" />
+
+                    <div class="self-start">
+                        by <a href="https://github.com/robbsnor" target="_blank" class="hover:underline">Robbsnor</a>
+                    </div>
                 </div>
             </div>
         </Container>
