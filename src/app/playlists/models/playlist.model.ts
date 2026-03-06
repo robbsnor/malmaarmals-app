70e35fd279c51c1c99f8914b2cb031b4b4b64c47
robbsnor <robbinschmidt@gmail.com>
2026-03-06 14:08:10 +0100

    -

--- src/app/playlists/models/playlist.model.ts
+++ src/app/playlists/models/playlist.model.ts
@@ -4,7 +4,7 @@ import { supabase } from '../../../supabase';
 export const playlistsQuery = supabase
     .from('playlists')
     .select(`*, videos:playlist_videos(...videos(*, chapters(*, category:categories(*))))`)
-    .order('position', { ascending: true });
+    .order('order', { ascending: true });
 
 export type Playlists = QueryData<typeof playlistsQuery>;
 export type Playlist = Playlists[number];
