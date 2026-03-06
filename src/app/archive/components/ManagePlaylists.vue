78bf56c416b706ffde00c4c1f7c3cafa99349ded
robbsnor <robbinschmidt@gmail.com>
2026-03-06 21:16:14 +0100

    -

---
+++ src/app/archive/components/ManagePlaylists.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import { usePlaylistsStore } from '../../playlists/stores/playlists.store';
+
+const playlistStore = usePlaylistsStore();
+</script>
+
+<template>
+    <div></div>
+</template>
