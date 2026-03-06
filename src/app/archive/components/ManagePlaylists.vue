0c4e663f37fd483319e935f0c2dcc902958a246a
robbsnor <robbinschmidt@gmail.com>
2026-03-07 00:36:59 +0100

    -

--- src/app/archive/components/ManagePlaylists.vue
+++ src/app/archive/components/ManagePlaylists.vue
@@ -3,8 +3,18 @@ import { computed, ref } from 'vue';
 import { usePlaylistsStore } from '../../playlists/stores/playlists.store';
 
 const playlistStore = usePlaylistsStore();
+
+const dialog = ref(false);
+
+function init() {}
 </script>
 
 <template>
-    <div></div>
+    <v-btn v-auth prepend-icon="mdi-pencil" color="primary" @click="dialog = true">Manage</v-btn>
+
+    <Dialog v-model="dialog" title="Manage Playlists" @open="init()">
+        <div v-for="playlist in playlistStore.playlists">
+            {{ playlist }}
+        </div>
+    </Dialog>
 </template>
