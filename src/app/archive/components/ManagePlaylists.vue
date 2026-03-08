46e75ea4e71b400e16dd3cc698ad2a06de16a7b7
robbsnor <robbinschmidt@gmail.com>
2026-03-08 20:50:19 +0100

    -

--- src/app/archive/components/ManagePlaylists.vue
+++ src/app/archive/components/ManagePlaylists.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { TitleHelper } from '../../shared/helpers/title.helper';
 import { usePlaylistsStore } from '../../playlists/stores/playlists.store';
-import PlaylistItem from '../../playlists/components/PlaylistItem.vue';
 import { computed, ref } from 'vue';
 import { useCloned } from '@vueuse/core';
 import { supabase } from '../../../supabase';
@@ -11,37 +10,52 @@ import { BucketHelper } from '../../shared/helpers/bucket.helper';
 
 const playlistsStore = usePlaylistsStore();
 
-const editMode = ref(false);
 const dialog = ref(false);
 const loading = ref(false);
 const { cloned, sync } = useCloned(() => playlistsStore.playlists);
 const hasChanges = computed(() => !_.isEqual(cloned.value, playlistsStore.playlists));
+const toDelete = computed(() => _.differenceBy(playlistsStore.playlists, cloned.value, 'id'));
 
 function onOpen() {
     sync();
 }
 
+function remove(index: number) {
+    cloned.value.splice(index, 1);
+}
+
 async function cancel() {
-    editMode.value = false;
     dialog.value = false;
 }
 
-async function save() {
-    loading.value = true;
+async function deletePlaylists() {
+    const ids = toDelete.value.map((p) => p.id);
+    await supabase.from('playlists').delete().in('id', ids);
+}
 
+async function reorderPlaylists() {
     const updates = cloned.value.map((playlist, index) => ({
         id: playlist.id,
         order: index,
     }));
-
-    await sleep(500);
     const promises = updates.map(({ id, order }) => supabase.from('playlists').update({ order }).eq('id', id));
     await Promise.all(promises);
-    await playlistsStore.fetchPlaylists();
+}
 
-    loading.value = false;
-    editMode.value = false;
-    dialog.value = false;
+async function save() {
+    try {
+        loading.value = true;
+
+        await deletePlaylists();
+        await reorderPlaylists();
+        await sleep(1500);
+        await playlistsStore.fetchPlaylists();
+    } catch (error) {
+        throw error;
+    } finally {
+        loading.value = false;
+        dialog.value = false;
+    }
 }
 </script>
 
@@ -57,7 +71,7 @@ async function save() {
             handle=".handle"
             v-model="cloned"
         >
-            <div v-for="playlist in cloned" :key="playlist.id" class="relative flex items-center py-2 px-4">
+            <div v-for="(playlist, i) in cloned" :key="playlist.id" class="relative flex items-center py-2 px-4">
                 <div class="flex items-center gap-4 flex-1">
                     <div class="w-18 rounded overflow-hidden">
                         <v-img :src="BucketHelper.getThumbnailUrl(playlist.videos[0]?.video_id)" alt="" />
@@ -73,18 +87,33 @@ async function save() {
                 </div>
 
                 <div class="flex justify-center items-center gap-2 pr-2">
-                    <v-btn icon="mdi-trash-can" variant="text" size="small" class="text-red-400! shrik-0"></v-btn>
+                    <v-btn
+                        icon="mdi-trash-can"
+                        variant="text"
+                        size="small"
+                        class="text-red-400! shrik-0"
+                        @click="remove(i)"
+                    ></v-btn>
+
                     <v-icon icon="mdi-drag" size="small" class="handle cursor-grab! text-muted! shrik-0"></v-icon>
                 </div>
             </div>
         </VueDraggable>
 
         <template #footer>
-            <v-btn class="text-muted!" variant="text" @click="cancel()"> cancel </v-btn>
+            <div class="flex justify-between items-center gap-4 w-full">
+                <div v-if="toDelete.length" class="italic text-muted text-sm">
+                    {{ toDelete.length }} playlists deleted
+                </div>
 
-            <v-btn color="primary" variant="tonal" @click="save()" :loading="loading" :disabled="!hasChanges">
-                save
-            </v-btn>
+                <div class="flex gap-4 ml-auto">
+                    <v-btn class="text-muted!" variant="text" @click="cancel()"> cancel </v-btn>
+
+                    <v-btn color="primary" variant="tonal" @click="save()" :loading="loading" :disabled="!hasChanges">
+                        save
+                    </v-btn>
+                </div>
+            </div>
         </template>
     </Dialog>
 </template>
