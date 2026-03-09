7376245b365418294cd4fd6fddb3acaee16eb55b
robbsnor <robbinschmidt@gmail.com>
2026-03-10 00:44:36 +0100

    -

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,8 @@ import { useHistoryStore } from './app/history/stores/history.store';
 import Offline from './app/layout/components/Offline.vue';
 import Footer from './app/layout/components/Footer.vue';
 import Header from './app/layout/components/Header.vue';
-import { useRoute } from 'vue-router';
+import BackgroundArt from './app/layout/components/BackgroundArt.vue';
 import { Analytics } from '@vercel/analytics/vue';
-
-const route = useRoute();
 
 const videosStore = useVideosStore();
 const authStore = useAuthStore();
@@ -59,6 +57,8 @@ onMounted(async () => {
 
 <template>
     <v-app v-if="!loading && !hasError">
+        <BackgroundArt />
+
         <Header />
 
         <div class="grid h-full pb-mobile-navbar lg:pt-header lg:pb-0" style="grid-template-rows: 1fr auto">
---
+++ src/app/layout/components/BackgroundArt.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="fixed h-1/2 w-full bg-black">
+        <Container class="relative h-full">
+            <div class="flex gap-4 absolute right-0 translate-x-1/6 h-full">
+                <div
+                    v-for="i in 3"
+                    :key="i"
+                    class="w-80 h-full pr-[1px] bg-gradient-to-br from-black to-primary/80 -skew-x-25"
+                >
+                    <div class="w-full h-full bg-gradient-to-br from-black to-black/20"></div>
+                </div>
+            </div>
+        </Container>
+
+        <div
+            class="absolute inset-0 bg-gradient-to-b from-transparent via-30% via-black/80 lg:via-black/20 to-black-100"
+        ></div>
+    </div>
+</template>
