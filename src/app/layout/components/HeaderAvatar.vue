d1095d0a0bc03fd59aa45d37f93c6b2231a957df
robbsnor <robbinschmidt@gmail.com>
2026-03-13 09:47:39 +0100

    -

--- src/app/layout/components/HeaderAvatar.vue
+++ src/app/layout/components/HeaderAvatar.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useAuthStore } from '../../auth/stores/auth.store';
+import NotSubscribedBadge from './NotSubscribedBadge.vue';
 
 const authStore = useAuthStore();
 const settingsDialog = ref(false);
@@ -17,7 +18,16 @@ function toggleSettingsDialog() {
                 v-bind="activator.props"
                 class="after:border-primary cursor-pointer relative inline-block h-7.5 w-7.5 after:absolute after:-inset-1 after:rounded-full after:border-2 after:content-['']"
             >
-                <img :src="authStore.session.user.user_metadata.avatar_url" alt="" class="h-full w-full rounded-full" />
+                <!-- <img :src="authStore.session.user.user_metadata.avatar_url" alt="" class="h-full w-full rounded-full" /> -->
+                <div v-if="!!authStore.session" class="relative">
+                    <img
+                        :src="authStore.session.user.user_metadata.avatar_url"
+                        alt=""
+                        class="h-8 cursor-pointer rounded-full"
+                    />
+
+                    <NotSubscribedBadge class="-right-2 -bottom-2" />
+                </div>
             </a>
         </template>
 
