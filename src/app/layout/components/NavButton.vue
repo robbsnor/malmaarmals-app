30add124e280a2b1481412e96f7638f8f0651966
robbsnor <robbinschmidt@gmail.com>
2026-03-15 08:41:06 +0100

    -

--- src/app/layout/components/NavButton.vue
+++ src/app/layout/components/NavButton.vue
@@ -3,13 +3,16 @@ const props = defineProps<{ to: any }>();
 </script>
 
 <template>
-    <v-btn
-        color="primary"
-        variant="text"
-        active-class="bg-red-600  text-white"
-        class="border-b-2 border-primary flex justify-center items-center px-4 py-2 font-bold capitalize transition-all text-muted"
-        v-bind="props"
+    <RouterLink
+        :to="props.to"
+        class="group relative h-full flex flex-col justify-center items-center px-4 uppercase font-bold text-primary-light"
     >
         <slot></slot>
-    </v-btn>
+        <div
+            class="absolute bottom-0 rounded-xl h-1 w-0 bg-primary opacity-0 group-hover:opacity-50 group-hover:w-full transition-all duration-100"
+        ></div>
+        <div
+            class="absolute bottom-0 h-[150%] translate-y-full opacity-0 aspect-square rounded-full bg-primary blur-xl -z-1 pointer-events-none transition-all duration-300 group-hover:translate-y-1/2 group-hover:opacity-60"
+        ></div>
+    </RouterLink>
 </template>
