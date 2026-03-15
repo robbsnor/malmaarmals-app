77a5cca653227d411c0a6d5bfd18f1d1c85417c0
robbsnor <robbinschmidt@gmail.com>
2026-03-16 00:50:39 +0100

    -

--- src/app/video/components/VideoThumbnail.vue
+++ src/app/video/components/VideoThumbnail.vue
@@ -14,7 +14,12 @@ const props = withDefaults(
 
 const historyStore = useHistoryStore();
 const history = computed(() => historyStore.history.find((v) => v.video_id === props.video.id));
-const progressionPercentage = computed(() => (100 / props.video.length_sec) * history.value?.video_time);
+const progressionPercentage = computed(() => {
+    const videoTime = Number(history.value?.video_time ?? 0);
+    if (!videoTime) return 0;
+
+    return (100 / props.video.length_sec) * videoTime;
+});
 const duration = computed(() => TimeHelper.formatTime(props.video.length_sec));
 </script>
 
@@ -22,7 +27,7 @@ const duration = computed(() => TimeHelper.formatTime(props.video.length_sec));
     <RouterLink :to="{ name: 'video', params: { id: props.video.video_id } }" class="relative">
         <Thumbnail :src="BucketHelper.getThumbnailUrl(props.video.video_id)">
             <div v-if="progressionPercentage" class="absolute bottom-0 right-0 left-0 h-1 z-1 bg-white/30">
-                <div class="bg-primary h-full" :style="{ width: `${progressionPercentage}%` }"></div>
+                <div class="bg-primary h-full" :style="{ width: `clamp(10%, ${progressionPercentage}%, 100%)` }"></div>
             </div>
 
             <div class="absolute bottom-2 right-2 bg-black/50 rounded-md text-xs px-1.5 py-0.5">
