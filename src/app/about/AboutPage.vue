8b822e68afde49b70d0122a90409fe93b3e1072f
robbsnor <robbinschmidt@gmail.com>
2026-03-16 19:28:13 +0100

    -

--- src/app/about/AboutPage.vue
+++ src/app/about/AboutPage.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue';
 import { TitleHelper } from '../shared/helpers/title.helper';
+import { useVideosStore } from '../video/stores/videos.store';
 
 TitleHelper.setTitle('about');
+const videosStore = useVideosStore();
 
 const faqs = ref([
     {
@@ -21,16 +23,64 @@ const faqs = ref([
 </script>
 
 <template>
-    <Container width="920px " class="py-4">
-        <div class="fs">
-            <h2>About Malmaarmals</h2>
-            <p>Malmaarmals is a website to archive and showcase old Twitch streams from the duo from Lekker Spelen.</p>
+    <div>
+        <Container width="920px " class="py-4">
+            <div class="fs">
+                <h2>About Malmaarmals</h2>
+                <p>
+                    Malmaarmals is a website to archive and showcase old Twitch streams from the duo from Lekker Spelen.
+                </p>
 
-            <h2>F.A.Q.</h2>
-            <v-expansion-panels>
-                <v-expansion-panel v-for="q in faqs" :key="q.question" :title="q.question" :text="q.answer">
-                </v-expansion-panel>
-            </v-expansion-panels>
-        </div>
-    </Container>
+                <h2>F.A.Q.</h2>
+                <v-expansion-panels>
+                    <v-expansion-panel v-for="q in faqs" :key="q.question" :title="q.question" :text="q.answer">
+                    </v-expansion-panel>
+                </v-expansion-panels>
+            </div>
+        </Container>
+
+        <Auth>
+            <Section title="Chapters overview">
+                <template #actions>
+                    <div class="text-muted">
+                        {{ videosStore.videos.filter((v) => v.chapters.length).length }} /
+                        {{ videosStore.videos.length }}
+                    </div>
+                </template>
+
+                <div class="grid grid-cols-[repeat(auto-fit,minmax(20px,1fr))] gap-1.5">
+                    <RouterLink
+                        v-for="video in videosStore.videos"
+                        :key="video.id"
+                        :to="{ name: 'video', params: { id: video.video_id } }"
+                        class="aspect-square rounded"
+                        :class="[video.chapters.length ? 'bg-green-500' : ' bg-black-400']"
+                    ></RouterLink>
+
+                    <!-- <v-menu
+                        v-for="video in videosStore.videos"
+                        :key="video.id"
+                        open-on-click
+                        open-on-hover
+                        open-delay="0"
+                        close-delay="0"
+                        location="top"
+                    >
+                        <template v-slot:activator="{ props }">
+                            <RouterLink
+                                v-bind="props"
+                                :to="{ name: 'video', params: { id: video.video_id } }"
+                                class="aspect-square rounded"
+                                :class="[video.chapters.length ? 'bg-green-500' : ' bg-black-400']"
+                            ></RouterLink>
+                        </template>
+
+                        <div class="bg-black-200 rounded border border-black-400 p-4">
+                            <div>{{ video.title }}</div>
+                        </div>
+                    </v-menu> -->
+                </div>
+            </Section>
+        </Auth>
+    </div>
 </template>
--- src/app/home/HomePage.vue
+++ src/app/home/HomePage.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
-import Stats from '../../app/home/components/Stats.vue';
 import Playlists from './components/Playlists.vue';
 import { TitleHelper } from '../shared/helpers/title.helper';
 import RecentStreams from './components/RecentStreams.vue';
 import History from './components/History.vue';
-import { useAuthStore } from '../auth/stores/auth.store';
-import { useVideoStore } from '../video/stores/video.store';
-import { useVideosStore } from '../video/stores/videos.store';
 
 TitleHelper.setTitle('home');
-const authStore = useAuthStore();
-const videosStore = useVideosStore();
 </script>
 
 <template>
@@ -25,49 +19,5 @@ const videosStore = useVideosStore();
         <RecentStreams />
         <Playlists />
         <History />
-
-        <Auth>
-            <Section title="Chapters overview">
-                <template #actions>
-                    <div class="text-muted">
-                        {{ videosStore.videos.filter((v) => v.chapters.length).length }} /
-                        {{ videosStore.videos.length }}
-                    </div>
-                </template>
-
-                <div class="grid grid-cols-[repeat(auto-fit,minmax(20px,1fr))] gap-1.5">
-                    <RouterLink
-                        v-for="video in videosStore.videos"
-                        :key="video.id"
-                        :to="{ name: 'video', params: { id: video.video_id } }"
-                        class="aspect-square rounded"
-                        :class="[video.chapters.length ? 'bg-green-500' : ' bg-black-400']"
-                    ></RouterLink>
-
-                    <!-- <v-menu
-                        v-for="video in videosStore.videos"
-                        :key="video.id"
-                        open-on-click
-                        open-on-hover
-                        open-delay="0"
-                        close-delay="0"
-                        location="top"
-                    >
-                        <template v-slot:activator="{ props }">
-                            <RouterLink
-                                v-bind="props"
-                                :to="{ name: 'video', params: { id: video.video_id } }"
-                                class="aspect-square rounded"
-                                :class="[video.chapters.length ? 'bg-green-500' : ' bg-black-400']"
-                            ></RouterLink>
-                        </template>
-
-                        <div class="bg-black-200 rounded border border-black-400 p-4">
-                            <div>{{ video.title }}</div>
-                        </div>
-                    </v-menu> -->
-                </div>
-            </Section>
-        </Auth>
     </div>
 </template>
