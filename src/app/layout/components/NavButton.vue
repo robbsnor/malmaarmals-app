0f0cec112a012117f2c40d8d2aa5cc50fb8666fc
robbsnor <robbinschmidt@gmail.com>
2026-03-17 02:19:04 +0100

    -

--- src/app/layout/components/NavButton.vue
+++ src/app/layout/components/NavButton.vue
@@ -8,16 +8,16 @@ const props = defineProps<{ to: RouteLocationRaw }>();
     <RouterLink
         :to="props.to"
         active-class="is-active"
-        class="group relative h-full flex flex-col justify-center items-center px-4 transition-colors uppercase font-bold text-muted [&.is-active]:text-primary-light"
+        class="group relative flex h-full flex-col items-center justify-center px-4 font-bold uppercase text-muted transition-colors [&.is-active]:text-primary-light"
     >
         <slot></slot>
 
         <div
-            class="absolute bottom-0 rounded-xl h-1 bg-primary transition-all duration-100 w-0 opacity-0 group-hover:opacity-50 group-hover:w-4 in-[.is-active]:opacity-100! in-[.is-active]:w-full!"
+            class="absolute bottom-0 h-0.5 w-0 rounded-xl bg-primary opacity-0 transition-all duration-100 group-hover:w-4 group-hover:opacity-50 in-[.is-active]:!w-full in-[.is-active]:!opacity-100"
         ></div>
 
         <div
-            class="absolute bottom-0 h-[150%] aspect-square rounded-full bg-primary blur-xl -z-1 pointer-events-none transition-all duration-300 translate-y-full opacity-0 group-hover:translate-y-2/3 group-hover:opacity-30 in-[.is-active]:!translate-y-1/2 in-[.is-active]:!opacity-60"
+            class="pointer-events-none absolute bottom-0 -z-1 aspect-square h-[100%] translate-y-full rounded-full bg-primary opacity-0 blur-xl transition-all duration-300 group-hover:translate-y-2/3 group-hover:opacity-30 in-[.is-active]:!translate-y-1/2 in-[.is-active]:!opacity-60"
         ></div>
     </RouterLink>
 </template>
