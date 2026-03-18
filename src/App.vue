6444dbf56402475b38116f6c2ce9bf7f5457a26e
robbsnor <robbinschmidt@gmail.com>
2026-03-18 13:11:09 +0100

    -

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ import Offline from './app/layout/components/Offline.vue';
 import Footer from './app/layout/components/Footer.vue';
 import Header from './app/layout/components/Header.vue';
 import BackgroundArt from './app/layout/components/BackgroundArt.vue';
+import SlowSiteBanner from './app/layout/components/SlowSiteBanner.vue';
 import { Analytics } from '@vercel/analytics/vue';
 
 const videosStore = useVideosStore();
@@ -65,6 +66,8 @@ onMounted(async () => {
         <Header />
 
         <div class="grid h-full pb-mobile-navbar lg:pt-header lg:pb-4" style="grid-template-rows: 1fr auto">
+            <SlowSiteBanner />
+
             <div>
                 <RouterView />
             </div>
