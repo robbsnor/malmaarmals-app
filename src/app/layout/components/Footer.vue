74d64539cd1844445c85760ecebf69c4c5aa6c29
robbsnor <robbinschmidt@gmail.com>
2026-03-18 12:55:44 +0100

    -

--- src/app/layout/components/Footer.vue
+++ src/app/layout/components/Footer.vue
@@ -7,8 +7,8 @@ const route = useRoute();
 <template>
     <div v-if="route.meta.showFooter !== false" class="border-t border-black-400">
         <Container>
-            <div class="flex justify-center items-center">
-                <div class="inline-grid grid-cols-[1fr_auto_1fr] gap-1 items-center text-muted-more py-10">
+            <div class="flex flex-col gap-4 justify-center items-center py-10">
+                <div class="inline-grid grid-cols-[1fr_auto_1fr] gap-1 items-center text-muted-more">
                     <div class="place-self-end">Made with</div>
 
                     <v-icon icon="mdi-heart-outline" class="text-primary animate-pulse -mt-1 mx-1" />
@@ -17,6 +17,14 @@ const route = useRoute();
                         by <a href="https://github.com/robbsnor" target="_blank" class="hover:underline">Robbsnor</a>
                     </div>
                 </div>
+
+                <a
+                    href="https://github.com/robbsnor/malmaarmals-app"
+                    target="_blank"
+                    class="text-muted-more hover:text-primary hover:scale-110"
+                >
+                    <v-icon>mdi-github</v-icon>
+                </a>
             </div>
         </Container>
     </div>
