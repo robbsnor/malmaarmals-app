47d396e1b6be168c33efb0a2399cae6283e78501
robbsnor <robbinschmidt@gmail.com>
2026-03-19 17:53:22 +0100

    -

--- src/app/home/components/PopularCategories.vue
+++ src/app/home/components/PopularCategories.vue
@@ -4,12 +4,18 @@ import { useRouter } from 'vue-router';
 import { useVideosStore } from '../../videos/stores/videos.store';
 import { useArchiveStore } from '../../archive/stores/archive.store';
 import CategoryCard from '../../categories/components/CategoryCard.vue';
+import { useDisplay } from 'vuetify';
 
 const videosStore = useVideosStore();
 const archiveStore = useArchiveStore();
 const router = useRouter();
+const { mdAndUp, lgAndUp, xlAndUp } = useDisplay();
 
-const preview = computed(() => videosStore.populairCategories.slice(0, 8));
+const amount = computed(() => {
+    if (mdAndUp.value) return 8;
+
+    return 6;
+});
 
 function selectCategory(title: string) {
     archiveStore.query = title;
@@ -23,8 +29,13 @@ function selectCategory(title: string) {
             <div class="text-muted-more font-bold">{{ videosStore.populairCategories.length }} categories</div>
         </template>
 
-        <div class="grid grid-cols-3 gap-3 md:grid-cols-4 lg:gap-8 xl:grid-cols-4 2xl:grid-cols-8">
-            <CategoryCard v-for="cat in preview" :key="cat.id" v-bind="cat" @click="selectCategory(cat.title)" />
+        <div class="grid grid-cols-3 gap-3 md:grid-cols-4 lg:gap-8 xl:grid-cols-8">
+            <CategoryCard
+                v-for="cat in videosStore.populairCategories.slice(0, amount)"
+                :key="cat.id"
+                v-bind="cat"
+                @click="selectCategory(cat.title)"
+            />
         </div>
     </Section>
 </template>
