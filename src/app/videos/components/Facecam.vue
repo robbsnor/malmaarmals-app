498b330bb505b95c5a76ae216fa04c04fddd833d
robbsnor <robbinschmidt@gmail.com>
2026-03-20 16:58:41 +0100

    -

--- src/app/videos/components/Facecam.vue
+++ src/app/videos/components/Facecam.vue
@@ -6,7 +6,7 @@ const preferenceStore = usePreferenceStore();
 
 <template>
     <div v-if="preferenceStore.showFacecam" class="flex absolute inset-0 items-center justify-center">
-        <div class="aspect-video w-full flex items-end justify-end p-4">
+        <div class="w-full h-full flex items-end justify-end p-4">
             <div class="relative aspect-video overflow-hidden rounded-md shadow-lg bg-black w-[30%] max-w-60">
                 <img src="/images/facecam2.jpg" class="object-fill w-full h-full" alt="Malse Facecam" />
             </div>
