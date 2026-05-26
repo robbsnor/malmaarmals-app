14691fc054d9ea40ec65a5448c26c5160e49b4a6
robbsnor <8747855+robbsnor@users.noreply.github.com>
2026-05-26 17:31:04 +0100

    .

--- src/App.vue
+++ src/App.vue
@@ -33,7 +33,7 @@ onMounted(async () => {
             await historyStore.fetchHistory();
         }
 
-        await sleep(900);
+        // await sleep(900);
     } catch (error) {
         await sleep(1500);
         appStore.hasError = true;
