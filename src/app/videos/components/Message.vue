b66e6dfffda6824d005c67569832829f9ebaa085
robbsnor <8747855+robbsnor@users.noreply.github.com>
2026-06-07 18:40:09 +0100

    -

--- src/app/videos/components/Message.vue
+++ src/app/videos/components/Message.vue
@@ -21,6 +21,7 @@ const isMyMessage = computed(
     () => props.message.user_id === Number(authStore.session.user?.user_metadata?.provider_id)
 );
 const isLekkerSpelen = computed(() => props.message.user_id === LEKKER_SPELEN_USER_ID);
+const isSub = computed(() => props.message.text.includes(`They've subscribed for`));
 </script>
 
 <template>
@@ -28,6 +29,8 @@ const isLekkerSpelen = computed(() => props.message.user_id === LEKKER_SPELEN_US
         :class="{
             'bg-black-300 rounded-sm py-1 -mx-1 px-1 md:bg-black-600 2xl:-mx-2 2xl:px-2':
                 (isMyMessage && props.highlightOwnMessage) || isLekkerSpelen,
+            'bg-gradient-to-r from-white/5 to-white/0 rounded-r p-2! border-l-primary border-l-2 rounded-l-none!':
+                isSub,
         }"
         class="text-sm"
     >
@@ -56,6 +59,7 @@ const isLekkerSpelen = computed(() => props.message.user_id === LEKKER_SPELEN_US
                 <a v-else-if="word.startsWith('http') || word.startsWith('https')" :href="word" target="_blank">
                     {{ word }}
                 </a>
+                <b v-else-if="isSub && word === 'months'">{{ word }}</b>
                 <template v-else>{{ word }}</template>
                 {{ ' ' }}
             </template>
