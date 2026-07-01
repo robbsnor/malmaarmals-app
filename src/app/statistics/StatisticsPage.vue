f6925350d513967aa6c27e5a4e50c924235ab744
robbsnor <8747855+robbsnor@users.noreply.github.com>
2026-07-01 14:54:17 +0100

    -

--- src/app/statistics/StatisticsPage.vue
+++ src/app/statistics/StatisticsPage.vue
@@ -1,23 +1,11 @@
 <script setup lang="ts">
-import { computed, ref, watch } from 'vue';
-import { useRouter } from 'vue-router';
-import { useVideosStore } from '../videos/stores/videos.store';
-import { useArchiveStore } from '../archive/stores/archive.store';
-import CategoryCard from '../categories/components/CategoryCardLarge.vue';
 import { TitleHelper } from '../shared/helpers/title.helper';
-import Empty from '../shared/components/Empty.vue';
-import { useStatsStore } from '../stats/stores/stats.store.ts';
 import WeekOverview from './components/WeekOverview.vue';
 import StreamHistoryOverview from './components/StreamHistoryOverview.vue';
 import ChaptersOverview from '../home/components/ChaptersOverview.vue';
 import WatchHistoryChart from './components/WatchHistoryChart.vue';
 
 TitleHelper.setTitle('statistics');
-
-const videosStore = useVideosStore();
-const statsStore = useStatsStore();
-const archiveStore = useArchiveStore();
-const router = useRouter();
 </script>
 
 <template>
