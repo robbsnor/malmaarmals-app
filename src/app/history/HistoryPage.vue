7f2e7beb3261f70ec60e22218be02808fa93f7c2
robbsnor <8747855+robbsnor@users.noreply.github.com>
2026-07-03 21:40:22 +0100

    -

--- src/app/history/HistoryPage.vue
+++ src/app/history/HistoryPage.vue
@@ -11,46 +11,40 @@ const dialog = ref(false);
 </script>
 
 <template>
-    <div>
-        <Container width="920px">
-            <div class="flex py-2 items-center gap-4 justify-between">
-                <v-btn variant="text" icon="mdi-chevron-left" :to="{ name: 'profile' }"></v-btn>
+    <MobilePage title="History" :to="{ name: 'profile' }">
+        <template #actions>
+            <v-menu v-if="historyStore.history.length">
+                <template v-slot:activator="{ props }">
+                    <v-btn icon="mdi-dots-vertical" variant="text" v-bind="props"></v-btn>
+                </template>
 
-                <v-menu v-if="historyStore.history.length">
-                    <template v-slot:activator="{ props }">
-                        <v-btn icon="mdi-dots-vertical" variant="text" v-bind="props"></v-btn>
-                    </template>
+                <v-list>
+                    <v-list-item @click="dialog = true" class="text-error" prepend-icon="mdi-trash-can-outline">
+                        <v-list-item-title>Clear history</v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </template>
 
-                    <v-list>
-                        <v-list-item @click="dialog = true" class="text-error" prepend-icon="mdi-trash-can-outline">
-                            <v-list-item-title>Clear history</v-list-item-title>
-                        </v-list-item>
-                    </v-list>
-                </v-menu>
-            </div>
-        </Container>
+        <div v-if="historyStore.history.length" class="flex flex-col gap-4">
+            <HistoryItem :video="video" v-for="video in historyStore.videos" :key="video.id" />
+        </div>
 
-        <Section title="History" class="pt-0" width="920px">
-            <div v-if="historyStore.history.length" class="flex flex-col gap-4">
-                <HistoryItem :video="video" v-for="video in historyStore.videos" :key="video.id" />
-            </div>
+        <Empty
+            v-else
+            title="No videos yet..."
+            description="Watched videos will appear here."
+            icon="mdi-history"
+        ></Empty>
 
-            <Empty
-                v-else
-                title="No videos yet..."
-                description="Watched videos will appear here."
-                icon="mdi-history"
-            ></Empty>
-
-            <DeleteDialog
-                v-model="dialog"
-                title="Clear history"
-                description="Are you sure you want to clear your history?"
-                confirmText="Clear"
-                icon="mdi-trash-can"
-                :showBody="false"
-                @confirm="historyStore.deleteAll"
-            />
-        </Section>
-    </div>
+        <DeleteDialog
+            v-model="dialog"
+            title="Clear history"
+            description="Are you sure you want to clear your history?"
+            confirmText="Clear"
+            icon="mdi-trash-can"
+            :showBody="false"
+            @confirm="historyStore.deleteAll"
+        />
+    </MobilePage>
 </template>
---
+++ src/app/shared/components/MobilePage.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import type { RouteLocationAsPathGeneric, RouteLocationAsRelativeGeneric } from 'vue-router';
+
+const props = withDefaults(
+    defineProps<{
+        title: string;
+        to: string | RouteLocationAsRelativeGeneric | RouteLocationAsPathGeneric;
+    }>(),
+    {}
+);
+</script>
+
+<template>
+    <Container width="920px">
+        <div
+            class="max-lg:fixed h-14 z-10 top-0 left-0 right-0 flex py-2 items-center gap-4 justify-between max-lg:bg-gradient-to-b from-black to-black/20 lg:-mx-4"
+        >
+            <div class="flex items-center gap-2">
+                <v-btn variant="text" icon="mdi-chevron-left" :to="props.to"></v-btn>
+                <div class="font-bold text-xl">{{ props.title }}</div>
+            </div>
+
+            <slot name="actions"></slot>
+        </div>
+
+        <div class="max-lg:pt-14">
+            <slot></slot>
+        </div>
+    </Container>
+</template>
