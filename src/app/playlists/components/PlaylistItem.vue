ffd5aeeb29d39d00328531394f4316b341230716
robbsnor <8747855+robbsnor@users.noreply.github.com>
2026-07-15 21:50:42 +0100

    -

--- src/app/playlists/components/PlaylistItem.vue
+++ src/app/playlists/components/PlaylistItem.vue
@@ -2,6 +2,7 @@
 import type { Playlist } from '../models/playlist.model';
 import { BucketHelper } from '../../shared/helpers/bucket.helper';
 import { useDisplay } from 'vuetify';
+import { computed } from 'vue';
 
 const props = withDefaults(
     defineProps<{
@@ -14,6 +15,11 @@ const props = withDefaults(
 );
 
 const { lgAndUp } = useDisplay();
+const categories = computed(() => {
+    const cats = props.playlist.videos.flatMap((video) => video.chapters.map((chapter) => chapter.category.title));
+    const uniqueCats = Array.from(new Set(cats));
+    return uniqueCats;
+});
 </script>
 
 <template>
@@ -51,6 +57,10 @@ const { lgAndUp } = useDisplay();
         <div class="pt-2">
             <h2 class="font-bold text-md">{{ playlist.title }}</h2>
             <div class="text-muted text-sm font-medium">{{ playlist.videos.length }} Videos</div>
+
+            <div v-if="categories.length" class="text-muted text-md font-medium line-clamp-2">
+                {{ categories.join(', ') }}
+            </div>
         </div>
     </RouterLink>
 </template>
